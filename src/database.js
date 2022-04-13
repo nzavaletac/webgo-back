f76b2ac830627c88ed0fbde8becc6d57485f8279
BraulioNR <branoleruiz@gmail.com>
2022-04-12 22:16:18 -0500

    Change in .env

--- src/database.js
+++ src/database.js
@@ -1,20 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const connect = () => {
   try {
-  
     mongoose.connect(
-      `mongodb://admin:${process.env.PASSWORD_DB}@cluster0-shard-00-00.7fitn.mongodb.net:27017,cluster0-shard-00-01.7fitn.mongodb.net:27017,cluster0-shard-00-02.7fitn.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zzacy4-shard-0&authSource=admin&retryWrites=true&w=majority`
-    );
+      `mongodb://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0-shard-00-00.7fitn.mongodb.net:27017,cluster0-shard-00-01.7fitn.mongodb.net:27017,cluster0-shard-00-02.7fitn.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zzacy4-shard-0&authSource=admin&retryWrites=true&w=majority`
+    )
     mongoose.connection.once("open", () => {
-      console.log("Database successfully connected");
-    });
+      console.log("Database successfully connected")
+    })
     mongoose.connection.on("error", () => {
-      console.log("Something went wrong");
-    });
+      console.log("Something went wrong")
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-module.exports = { connect };
+module.exports = { connect }
