6fca23ee75d5b7f8032327f40710415357d3317d
Noe Zavaleta C <nzavaletac@gmail.com>
2022-04-20 17:34:21 -0500

    Update heroku

--- src/server.js
+++ src/server.js
@@ -8,25 +8,23 @@ const eventRouter = require("./routes/events.route");
 const categoriesRouter = require("./routes/categories.route");
 const { auth } = require("./utils/auth");
 
-
 const port = process.env.PORT || 8000;
 const app = express();
 connect();
 
-app.use(express.json({ limit: "100mb" }))
-app.use(cors())
+app.use(express.json({ limit: "100mb" }));
+app.use(cors());
 app.use(morgan("dev"));
 
-app.get("/", auth, (req, res) => {
-  console.log(req.userId);
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "It´s working",
   });
 });
 
-app.use("/users", userRouter)
-app.use("/events", eventRouter)
-app.use("/categories", categoriesRouter)
+app.use("/users", userRouter);
+app.use("/events", eventRouter);
+app.use("/categories", categoriesRouter);
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
