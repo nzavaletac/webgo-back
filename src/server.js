9372c1cd781f801c5fd6b638c560688c2fb2b88c
BraulioNR <53287676+BraulioNR@users.noreply.github.com>
2022-04-20 17:35:01 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "100mb" }))
 app.use(cors())
 app.use(morgan("dev"));
 
-app.get("/", auth, (req, res) => {
+app.get("/", (req, res) => {
   console.log(req.userId);
   res.status(200).json({
     message: "It´s working",
